b393fd00da4f766f5dc9a0f2891610a8cb8f3b97
Gustavo Guichard <gustavoguichard@gmail.com>
2017-12-06 23:09:42 -0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import numeral from 'numeral';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+numeral.register('locale', 'pt-BR', {
+    delimiters: {
+        thousands: '.',
+        decimal: ','
+    },
+});
+numeral.locale('pt-BR');
+
+class App extends Component {
+  state = {
+    coindesk: 0,
+    coindeskBR: 0,
+    mercado: 0,
+  }
+
+  componentWillMount() {
+    this.fetchCoindesk()
+    this.fetchMercado()
+  }
+
+  fetchCoindesk() {
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice/BRL.json')
+      .then(({ data }) => {
+        const { BRL, USD } = data.bpi
+        if(BRL && USD) {
+          this.setState({ coindesk: USD.rate_float, coindeskBR: BRL.rate_float })
+        }
+      })
+  }
+
+  fetchMercado() {
+    axios.get('https://www.mercadobitcoin.net/api/BTC/ticker/')
+      .then(({ data }) => {
+        const { last } = data.ticker
+        if(last) {
+          const mercado = parseFloat(last)
+          this.setState({ mercado })
+        }
+      })
+  }
+
+  get prices() {
+    const format = '0,0.00'
+    return {
+      coindesk: numeral(this.state.coindesk).format(format),
+      mercado: numeral(this.state.mercado).format(format),
+    }
+  }
+
+  get agio() {
+    const total = ((this.state.mercado * 100) / this.state.coindeskBR) - 100
+    return numeral(total).format('0.0')
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Bitcoin Ágio: {this.agio}%</h1>
+        </header>
+        <p className="App-intro">
+          MercadoBitcoin: R$ {this.prices.mercado}
+        </p>
+        <p className="App-intro">
+          Coindesk: U$ {this.prices.coindesk}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
